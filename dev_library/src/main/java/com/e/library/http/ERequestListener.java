b0dc0368be6bcef07478b8d9324f0cc0da3a7588
E <linxueyi2230@gmail.com>
2016-08-24 01:41:21 +0800

    add NetWorkHelper

--- dev_library/src/main/java/com/e/library/http/ERequestListener.java
+++ dev_library/src/main/java/com/e/library/http/ERequestListener.java
@@ -5,11 +5,11 @@ package com.e.library.http;
  */
 public abstract class ERequestListener {
 
-    public void onStart(){};
+    public void onStart(){}
 
-    public void onLoading(long count,long current){};
+    public void onLoading(long count,long current){}
 
     public abstract <R extends EResponse> void onFinish(R response);
 
-    public void onFailure(Throwable t,int errorNo ,String strMsg){};
+    public void onFailure(Throwable t,int errorNo ,String strMsg){}
 }
